9b8d2611c2a785c3e6684a9192d8ced49547771e
embarq <embarq.dev@gmail.com>
2025-02-16 19:40:27 +0900

    feat i18n: map out sign in page

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -3,39 +3,45 @@ import { FormMessage, Message } from '@/components/form-message'
 import { SubmitButton } from '@/components/submit-button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { getTranslations } from 'next-intl/server'
 import Link from 'next/link'
 
 export default async function Login(props: { searchParams: Promise<Message> }) {
   const searchParams = await props.searchParams
+  const t = await getTranslations('SignIn')
+
   return (
     <form className="flex min-w-64 flex-1 flex-col">
-      <h1 className="text-2xl font-medium">Sign in</h1>
+      <h1 className="text-2xl font-medium">{t('title')}</h1>
       <p className="text-sm text-foreground">
-        Don't have an account?{' '}
+        {t('sign_up_hint')}{' '}
         <Link className="font-medium text-foreground underline" href="/sign-up">
-          Sign up
+          {t('sign_up_link')}
         </Link>
       </p>
       <div className="mt-8 flex flex-col gap-2 [&>input]:mb-3">
-        <Label htmlFor="email">Email</Label>
+        <Label htmlFor="email">{t('email_field_label')}</Label>
         <Input name="email" placeholder="you@example.com" required />
         <div className="flex items-center justify-between">
-          <Label htmlFor="password">Password</Label>
+          <Label htmlFor="password">{t('password_field_label')}</Label>
           <Link
             className="text-xs text-foreground underline"
             href="/forgot-password"
           >
-            Forgot Password?
+            {t('forgot_password_link')}
           </Link>
         </div>
         <Input
           type="password"
           name="password"
-          placeholder="Your password"
+          placeholder={t('password_field_placeholder')}
           required
         />
-        <SubmitButton pendingText="Signing In..." formAction={signInAction}>
-          Sign in
+        <SubmitButton
+          pendingText={t('sign_in_button_loading')}
+          formAction={signInAction}
+        >
+          {t('sign_in_button')}
         </SubmitButton>
         <FormMessage message={searchParams} />
       </div>
