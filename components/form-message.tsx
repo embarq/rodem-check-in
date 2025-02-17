65152f9a8799db084769c0c84f5d706213c2c621
embarq <embarq.dev@gmail.com>
2025-02-18 02:21:54 +0900

    feat: improve ui; add title prop

--- components/form-message.tsx
+++ components/form-message.tsx
@@ -6,33 +6,45 @@ export type Message =
   | { message: string }
 
 interface Props {
+  title?: string
   message?: Message | null
   className?: string
 }
 
-export function FormMessage({ message, className }: Props) {
+export function FormMessage({ title, message, className }: Props) {
   if (!message) return null
 
   return (
     <div
       className={classNames(
-        'flex w-full max-w-md gap-2 mt-3 flex-col text-sm',
+        'relative flex flex-col w-full max-w-md mt-3 p-2 border-l-4 text-foreground',
+        {
+          'border-green-400': 'success' in message,
+          'border-destructive': 'error' in message,
+        },
         className,
       )}
     >
-      {'success' in message && (
-        <div className="border-l-4 border-green-400 px-2 text-foreground">
-          {message.success}
-        </div>
-      )}
-      {'error' in message && (
-        <div className="border-l-4 border-destructive px-2 text-destructive">
-          {message.error}
-        </div>
-      )}
-      {'message' in message && (
-        <div className="border-l-4 px-2 text-foreground">{message.message}</div>
-      )}
+      <div
+        className={classNames(
+          'absolute inset-0 -z-10 bg-gradient-to-r via-transparent to-transparent opacity-10',
+          {
+            'from-green-500': 'success' in message,
+            'from-destructive': 'error' in message,
+          },
+        )}
+      ></div>
+      <strong
+        className={classNames('text-base font-bold', {
+          'text-green-500': 'success' in message,
+          'text-destructive': 'error' in message,
+        })}
+      >
+        {title}
+      </strong>
+      {'success' in message && <p>{message.success}</p>}
+      {'error' in message && <p>{message.error}</p>}
+      {'message' in message && <p>{message.message}</p>}
     </div>
   )
 }
