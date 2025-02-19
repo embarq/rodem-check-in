4daaed22ef6c9a0bf26486ccb0bb4641beb31318
embarq <embarq.dev@gmail.com>
2025-02-19 10:56:50 +0900

    feat core: update check-in geopositioning handling

--- lib/config.ts
+++ lib/config.ts
@@ -1,11 +1,29 @@
-export const checkInTargetGeolocation =
-  process.env.NEXT_PUBLIC_TARGET_GEOLOCATION
+import { z } from 'zod'
 
-export const checkInTargetMaxDistance = parseInt(
-  process.env.NEXT_PUBLIC_TARGET_MAX_DISTANCE ?? '0',
+export const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3033'
+
+export const checkInTargetGeolocation = parseTargetGeolocation(
+  process.env.TARGET_GEOLOCATION,
 )
 
 export const checkInDebounceMinutes = parseInt(
-  process.env.NEXT_PUBLIC_CHECK_IN_DEBOUNCE_MINUTES ?? '0',
+  process.env.CHECK_IN_DEBOUNCE_MINUTES ?? '0',
 )
+
+function parseTargetGeolocation(value?: string) {
+  const src = Buffer.from(z.string().base64().parse(value), 'base64').toString(
+    'utf-8',
+  )
+
+  return z
+    .object({
+      n: z.tuple([z.number(), z.number()]),
+      e: z.tuple([z.number(), z.number()]),
+      s: z.tuple([z.number(), z.number()]),
+      w: z.tuple([z.number(), z.number()]),
+    })
+    .parse(JSON.parse(src))
+}
 
