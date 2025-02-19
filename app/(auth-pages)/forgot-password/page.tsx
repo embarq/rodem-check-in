125d9119517c1366960f48e307c460a549a8e04d
embarq <embarq.dev@gmail.com>
2025-02-19 10:56:07 +0900

    feat ui: use the same layout configuration for auth pages

--- app/(auth-pages)/forgot-password/page.tsx
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { getTranslations } from 'next-intl/server'
+import { CommonLayout } from '@/app/(auth-pages)/common-layout'
 import { forgotPasswordAction } from '@/app/actions'
 import { FormMessage } from '@/components/form-message'
 import { SubmitButton } from '@/components/submit-button'
@@ -16,25 +17,24 @@ export default async function ForgotPassword(props: {
   const formMessage = maybeTranslateFormMessage(searchParams, t)
 
   return (
-    <form className="mx-auto flex w-full min-w-64 max-w-64 flex-1 flex-col gap-2 text-foreground [&>input]:mb-6">
-      <div>
-        <h1 className="text-2xl font-medium">{t('title')}</h1>
-        <p className="text-sm text-secondary-foreground">
+    <CommonLayout
+      title={t('title')}
+      description={
+        <>
           {t('sign_in_hint')}{' '}
           <Link className="text-primary underline" href="/sign-in">
             {t('sign_in_link')}
           </Link>
-        </p>
-      </div>
-      <div className="mt-8 flex flex-col gap-2 [&>input]:mb-3">
-        <Label htmlFor="email">{t('email_field_label')}</Label>
-        <Input name="email" placeholder="you@example.com" required />
-        <SubmitButton formAction={forgotPasswordAction}>
-          {t('reset_password_button')}
-        </SubmitButton>
-        <FormMessage message={formMessage} />
-      </div>
-    </form>
+        </>
+      }
+    >
+      <Label htmlFor="email">{t('email_field_label')}</Label>
+      <Input name="email" placeholder="you@example.com" required />
+      <SubmitButton formAction={forgotPasswordAction}>
+        {t('reset_password_button')}
+      </SubmitButton>
+      <FormMessage message={formMessage} />
+    </CommonLayout>
   )
 }
 
